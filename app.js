0a8f77d5f506dd5ad1b0441d9ad76ab68a271c3a
jewels98 <mrwright0825@gmail.com>
2018-06-21 16:10:09 -0400

    Assignment 7: added delete button

--- app.js
+++ app.js
@@ -4,22 +4,29 @@ function onReady() {
     const toDoList = document.getElementById('toDoList');
 
     //addToDoForm.addEventListener();
-    addToDoForm.addEventListner('submit', ()=> {
-event.preventDefault();
+addToDoForm.addEventListner('submit', (event) => {
+    event.preventDefault();
 
-let title = newTodotext.value;
+let title = newToDoText.value;
 let newLi = document.createElement('li');
 let checkbox = document.createElement('input');
+let deleteButton = document.createElement('button');
 checkbox.type = "checkbox";
+deleteButton.textContent = 'delete';
 
 newLi.textContent = title;
 newLi.appendChild(checkbox);
 toDoList.appendChild(newLi);
+toDoList.appendChild(deleteButton);
 newToDoText.value = '';
+
+deleteButton.onclick = () => {
+    toDoList.removeChild(newLi);
+    toDoList.removeChild(deleteButton);
+};
     });
 }
 
 window.onload = function(){
-    //alert("The window has loaded!");
     onReady();
 };

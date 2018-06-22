6fde0459723a9a6248ecb52a2758fb337d8778a0
jewels98 <mrwright0825@gmail.com>
2018-06-22 15:47:43 -0400

    Fixed undefined window error: assignment complete

--- app.js
+++ app.js
@@ -3,30 +3,33 @@ function onReady() {
     const newToDoText = document.getElementById('newToDoText');
     const toDoList = document.getElementById('toDoList');
 
-    //addToDoForm.addEventListener();
-addToDoForm.addEventListner('submit', (event) => {
-    event.preventDefault();
+    addToDoForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        let title = newToDoText.value;
+        let newLi = document.createElement('li');
+        let checkbox = document.createElement('input');
+        let deleteButton = document.createElement('input')
+        deleteButton.className = 'dltbtn';
+        deleteButton.type = 'button';
+        deleteButton.name = 'delbtn';
+        deleteButton.value = "Delete";
+        checkbox.type = "checkbox";
+        newLi.textContent = title;
+        toDoList.appendChild(newLi);
+        newLi.appendChild(checkbox);
+        newLi.appendChild(deleteButton);
+        newToDoText.value = '';
 
-let title = newToDoText.value;
-let newLi = document.createElement('li');
-let checkbox = document.createElement('input');
-let deleteButton = document.createElement('button');
-checkbox.type = "checkbox";
-deleteButton.textContent = 'delete';
-
-newLi.textContent = title;
-newLi.appendChild(checkbox);
-toDoList.appendChild(newLi);
-toDoList.appendChild(deleteButton);
-newToDoText.value = '';
-
-deleteButton.onclick = () => {
-    toDoList.removeChild(newLi);
-    toDoList.removeChild(deleteButton);
-};
+        deleteButton.addEventListener('click', (event) => {
+            event.preventDefault();
+            toDoList.removeChild(newLi);
+        });
     });
 }
 
-window.onload = function(){
+
+window.onload = function () {
+    //alert("The window has loaded!");
     onReady();
 };
+
